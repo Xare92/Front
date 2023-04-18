3f6fa6d05823b7aa3d65a9e3835104e106d8e82c
Xare92 <126160220+Xare92@users.noreply.github.com>
2023-04-18 17:31:13 +0200

    Add files via upload

---
+++ FronEnd/src/bottone.js
@@ -0,0 +1,22 @@
+function startpagina(){
+    let strpaginainiziale = '<input type="button" value="Premi Qui" onclick="gestioneArray()">';
+    document.getElementById("idMainContent").innerHTML=strpaginainiziale;
+}
+
+
+function gestioneArray(elementoCercare){
+    let myArray = [1,23,5,7,12];
+    
+}
+function findElement(){
+    
+}
+function sommaArray(){
+
+}
+function stampaArray(){
+
+}
+function stampaInversa(){
+
+}
